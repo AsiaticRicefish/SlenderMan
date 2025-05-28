adff046a13c3f0e27e06c24fb723a584fa931e34
AsiaticRicefish <125473806+AsiaticRicefish@users.noreply.github.com>
2025-05-28 17:06:39 +0900

    [feat] 배터리 기능 추가
    1. 배터리 소모하면 켜지지 않도록 구현
    2. UI와 연결하여 현재 배터리 상태 확인

--- Assets/Scripts/Players/FlashlightController.cs
+++ Assets/Scripts/Players/FlashlightController.cs
@@ -1,29 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FlashlightController : MonoBehaviour
 {
     [SerializeField] Light flashlight;
     [SerializeField] AudioSource switchSound;
+    [SerializeField] float maxBatteryTime = 60f;
+    [SerializeField] Image[] batteryBars; // 배터리 UI 칸
+
+    private float currentBatteryTime;
+    private bool isOn;  // 손전등이 켜져 있는 상태
 
     private void Start()
     {
+        currentBatteryTime = maxBatteryTime;
         flashlight.enabled = false;
     }
 
     private void Update()
     {
         FlashOnOff();
+
+        if (isOn) DrainBattery(); // 켜져 있으면 배터리를 소모
     }
 
     private void FlashOnOff()
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-            flashlight.enabled = !flashlight.enabled;
+            // 배터리가 없으면 켜지지 않음
+            if (currentBatteryTime <= 0f) return;
 
-            switchSound.Play();
+            isOn = !isOn;
+            flashlight.enabled = isOn;
+
+            if (switchSound && !switchSound.isPlaying)
+            {
+                switchSound.Play();
+            }
+        }
+    }
+
+    private void DrainBattery()
+    {
+        currentBatteryTime -= Time.deltaTime;  // 손전등이 켜져 있는 동안 매 프레임마다 배터리 시간 감소
+
+        if (currentBatteryTime <= 0f)  // 배터리 다 닳으면 손전등 강제로 꺼짐
+        {
+            currentBatteryTime = 0f;
+            flashlight.enabled = false;
+            isOn = false;
+        }
+
+        UpdateBatteryUI();
+    }
+
+    private void UpdateBatteryUI()
+    {
+        float percent = currentBatteryTime / maxBatteryTime; // 현재 배터리의 비율(%)을 계산하여 한칸씩 없어지도록 만듦
+
+        for (int i = 0; i < batteryBars.Length; i++)
+        {
+            batteryBars[i].enabled = percent > (float)i / batteryBars.Length;
         }
     }
 }
