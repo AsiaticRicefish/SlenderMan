49028b5d602d25a79f87ab190a3ada10c55c6a72
AsiaticRicefish <125473806+AsiaticRicefish@users.noreply.github.com>
2025-05-30 12:26:53 +0900

    [fix] 아이템 버그 수정
    아이템이 연속으로 먹어지는 현상 발견
    → 쿨타임을 걸어 연속으로 먹어지는 현상 제어

--- Assets/Scripts/Players/PlayerInteraction.cs
+++ Assets/Scripts/Players/PlayerInteraction.cs
@@ -11,6 +11,9 @@ public class PlayerInteraction : MonoBehaviour
     [SerializeField] Camera cam;
     [SerializeField] TMP_Text interactionText;
 
+    [SerializeField] float interactionCooldown = 0.5f; // 연속으로 아이템 획득 방지 쿨타임
+    private float lastInteractionTime = float.MinValue; // 게임 시작 직후에도 쿨타임 없이 바로 상호작용 가능 (float.MinValue는 아주 작은 수)
+
     private void Update()
     {
         Ray ray = new Ray(cam.transform.position, cam.transform.forward);
@@ -22,8 +25,10 @@ public class PlayerInteraction : MonoBehaviour
                 interactionText.text = item.GetInteractionText();
                 interactionText.enabled = true;
 
-                if (Input.GetKeyDown(KeyCode.E))
+                // 쿨타임 체크 (연속으로 아이템이 먹어지는 부분을 수정)
+                if (Input.GetKeyDown(KeyCode.E) && Time.time - lastInteractionTime > interactionCooldown)
                 {
+                    lastInteractionTime = Time.time;
                     item.Interact();
                 }
                 return;
