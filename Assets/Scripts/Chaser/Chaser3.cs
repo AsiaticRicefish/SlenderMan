0bb50a6adceaabb150cfabf5fbde2b65248e0799
AsiaticRicefish <125473806+AsiaticRicefish@users.noreply.github.com>
2025-06-02 20:00:59 +0900

    [feat] 세번째 추격자 생성
    첫번째, 두번째 추격자와 달리 플레이어를 계속 따라오는 일반 기믹으로 구성함

---
+++ Assets/Scripts/Chaser/Chaser3.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Chaser3 : MonoBehaviour
+{
+    private Transform player;
+    private NavMeshAgent agent;
+    private Animator animator;
+    [SerializeField] float gameOverDistance = 1f;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
+        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+        if (player == null)
+        {
+            player = playerObj.transform;
+        }
+    }
+
+    private void Update()
+    {
+        if (player != null)
+        {
+            agent.SetDestination(player.position);
+
+            float speed = agent.velocity.magnitude;
+            bool isWalking = speed > 0.1f;
+
+            if (animator != null)
+            {
+                animator.SetBool("IsWalk", isWalking);
+            }
+
+            float distance = Vector3.Distance(transform.position, player.position);
+            if (distance <= gameOverDistance)
+            {
+                GameOver();
+            }
+        }
+    }
+
+    private void GameOver()
+    {
+        if (GameManager._instance != null)
+        {
+            GameManager._instance.GameOver();
+        }
+    }
+
+}
