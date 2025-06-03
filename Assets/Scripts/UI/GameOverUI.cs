a021eca7dcd9b1e77df8cc514b5dc844d4b85437
AsiaticRicefish <125473806+AsiaticRicefish@users.noreply.github.com>
2025-06-03 14:53:15 +0900

    [rename] 클래스 이름 수정

--- Assets/Scripts/UI/GameOverUI.cs
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameOverUIManager : MonoBehaviour
+public class GameOverUI : MonoBehaviour
 {
     public void BackToMainMenu()
     {
